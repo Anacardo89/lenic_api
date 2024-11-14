b4d4b72964da861fb06b188576a837533c02073a
anacardo <pedro.fortunato.89@gmail.com>
2024-11-14 15:31:51 +0000

    API tested until RatePost

--- internal/interceptor/streamInterceptor.go
+++ internal/interceptor/streamInterceptor.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"reflect"
 
 	"github.com/Anacardo89/lenic_api/internal/pb"
 	"github.com/Anacardo89/lenic_api/pkg/auth"
@@ -25,7 +26,9 @@ func AuthStreamInterceptor(srv interface{}, ss grpc.ServerStream, info *grpc.Str
 
 	method := info.FullMethod
 
-	req, err := extractRequestFromStream(method)
+	bs := &BufferedStream{ServerStream: ss}
+
+	req, err := extractRequestFromStream(bs, method)
 	if err != nil {
 		return fmt.Errorf("coul not get request from stream: %v", err)
 	}
@@ -34,7 +37,7 @@ func AuthStreamInterceptor(srv interface{}, ss grpc.ServerStream, info *grpc.Str
 		uuid := getUUIDFromRequest(req)
 
 		if getIsPublic(uuid) {
-			return handler(srv, ss)
+			return handler(srv, bs)
 		} else {
 			if !isSelfRequest(claims.Username, req) && !isFollowerRequest(claims.FollowingIDs, req) {
 				return status.Errorf(codes.PermissionDenied, "access denied for private post")
@@ -51,7 +54,7 @@ func AuthStreamInterceptor(srv interface{}, ss grpc.ServerStream, info *grpc.Str
 			return status.Errorf(codes.PermissionDenied, "access restricted to followers")
 		}
 	}
-	return handler(srv, ss)
+	return handler(srv, bs)
 }
 
 func extractClaimsFromContext(ctx context.Context) (*auth.Claims, error) {
@@ -79,11 +82,17 @@ func extractClaimsFromContext(ctx context.Context) (*auth.Claims, error) {
 
 }
 
-func extractRequestFromStream(method string) (proto.Message, error) {
+func extractRequestFromStream(bs *BufferedStream, method string) (proto.Message, error) {
 	req, err := extractRequestType(method)
 	if err != nil {
 		return nil, err
 	}
+
+	err = bs.RecvMsg(req)
+	if err != nil {
+		return nil, fmt.Errorf("could not read message from stream: %v", err)
+	}
+
 	return req, nil
 }
 
@@ -110,4 +119,28 @@ func extractRequestType(method string) (proto.Message, error) {
 	default:
 		return nil, fmt.Errorf("unknown method: %s", method)
 	}
+}
+
+type BufferedStream struct {
+	grpc.ServerStream
+	bufferedRequest proto.Message
+}
+
+func (bs *BufferedStream) RecvMsg(m interface{}) error {
+	// If the message is already buffered, return it without consuming the stream
+	if bs.bufferedRequest != nil {
+		// Clone the buffered request into the provided message.
+		reflect.ValueOf(m).Elem().Set(reflect.ValueOf(bs.bufferedRequest).Elem())
+		return nil
+	}
+
+	// Otherwise, read the message from the stream normally
+	err := bs.ServerStream.RecvMsg(m)
+	if err != nil {
+		return err
+	}
+
+	// Buffer the received message for future access without consuming the stream
+	bs.bufferedRequest = m.(proto.Message)
+	return nil
 }
